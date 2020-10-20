a1b9eee4e3e6f0f26c7601ade401ef3cd1cad90a
deasynabila <deasy_nabila_29rpl@student.smktelkom-mlg.sch.id>
2020-10-20 19:10:04 +0700

    oke berhasil

---
+++ src/kartupelajar/Kartupelajar.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kartupelajar;
+
+/**
+ *
+ * @author MOKLET-1
+ */
+public class Kartupelajar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nama, alamat, nisn, tanggallahir;
+        
+        nama = "Deasy Nabila Nur Safitri";
+        nisn = "0046177653";
+        tanggallahir = "03-12-2004";
+        alamat = "Jl.Simpang Teluk Bayur 10,Blimbing";
+        
+        System.out.println("<<< KARTU PELAJAR >>>");
+        System.out.println("nama: "+ nama);
+        System.out.println("nisn: "+ nisn);
+        System.out.println("tanggal lahir: "+ tanggallahir);
+        System.out.println("alamat: "+ alamat); 
+    }
+    
+}
